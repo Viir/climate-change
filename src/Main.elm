d394fd6b846fc9c66dfb87eca4be9e501d8b6d3a
Michael Rätzel <Viir@Viir.de>
2019-06-29 14:56:57 +0000

    Remove obsolete parts

--- src/Main.elm
+++ src/Main.elm
@@ -24,28 +24,12 @@ waterLevel =
     (displaySizeY * 3) // 4
 
 
-type SnakeDirection
-    = Up
-    | Right
-    | Down
-    | Left
-
-
 type alias GameState =
-    { snake : Snake
-    , appleLocation : Location
-    }
+    {}
 
 
 type alias Location =
     { x : Int, y : Int }
-
-
-type alias Snake =
-    { headDirection : SnakeDirection
-    , headLocation : Location
-    , tailSegments : List Location
-    }
 
 
 main : SimpleGame GameState ()
@@ -63,52 +47,12 @@ main =
 
 initialState : GameState
 initialState =
-    { snake = { headDirection = Right, headLocation = { x = 4, y = 5 }, tailSegments = [ { x = 3, y = 5 } ] }
-    , appleLocation = { x = 3, y = 2 }
-    }
-
-
-snakeDirectionFromKeyboardKey : List ( Keyboard.Key.Key, SnakeDirection )
-snakeDirectionFromKeyboardKey =
-    [ ( Keyboard.Key.W, Up )
-    , ( Keyboard.Key.A, Left )
-    , ( Keyboard.Key.S, Down )
-    , ( Keyboard.Key.D, Right )
-    , ( Keyboard.Key.Up, Up )
-    , ( Keyboard.Key.Down, Down )
-    , ( Keyboard.Key.Left, Left )
-    , ( Keyboard.Key.Right, Right )
-    ]
+    {}
 
 
 onKeyDown : KeyboardEvent -> GameState -> GameState
 onKeyDown keyboardEvent gameStateBefore =
-    case snakeDirectionFromKeyboardKey |> listDictGet keyboardEvent.keyCode of
-        Nothing ->
-            gameStateBefore
-
-        Just snakeDirection ->
-            let
-                snakeBefore =
-                    gameStateBefore.snake
-            in
-            { gameStateBefore | snake = { snakeBefore | headDirection = snakeDirection } }
-
-
-xyOffsetFromDirection : SnakeDirection -> { x : Int, y : Int }
-xyOffsetFromDirection direction =
-    case direction of
-        Up ->
-            { x = 0, y = -1 }
-
-        Down ->
-            { x = 0, y = 1 }
-
-        Left ->
-            { x = -1, y = 0 }
-
-        Right ->
-            { x = 1, y = 0 }
+    gameStateBefore
 
 
 moveSnakeForwardOneStep : GameState -> GameState
@@ -119,28 +63,6 @@ moveSnakeForwardOneStep gameStateBefore =
 renderToHtml : GameState -> Html.Html ()
 renderToHtml gameState =
     let
-        cellSideLength =
-            30
-
-        svgRectAtCellLocation fill cellLocation =
-            svgRectFrom_Fill_Left_Top_Width_Height
-                fill
-                ( cellLocation.x * cellSideLength + 1
-                , cellLocation.y * cellSideLength + 1
-                )
-                ( cellSideLength - 2
-                , cellSideLength - 2
-                )
-
-        snakeView =
-            [ gameState.snake.headLocation ]
-                ++ gameState.snake.tailSegments
-                |> List.map (svgRectAtCellLocation "whitesmoke")
-                |> Svg.g []
-
-        appleView =
-            svgRectAtCellLocation "red" gameState.appleLocation
-
         skyHtml =
             svgRectFrom_Fill_Left_Top_Width_Height "skyblue" ( 0, 0 ) ( 1000, 1000 )
 
